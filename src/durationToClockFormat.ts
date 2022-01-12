786841d339502d8ded5837eaaa60d442ab4603fd
Carsten Daurehøj <arelstone@gmail.com>
2022-01-12 11:03:11 +0100

    add durationToClockFormat

---
+++ src/durationToClockFormat.ts
@@ -0,0 +1,11 @@
+export const durationToClockFormat = (duration: number): string | null | void => {
+  if (!duration) return null
+
+  const h = Math.floor(duration / 3600)
+  const m = Math.floor((duration % 3600) / 60)
+  const s = duration % 60
+
+  if (h > 0) return `${`00${h}`.slice(-2)}:${`00${m}`.slice(-2)}:${`00${s}`.slice(-2)}`
+  if (m > 0) return `${`00${m}`.slice(-2)}:${`00${s}`.slice(-2)}`
+  if (s > 0) return `00:${`00${s}`.slice(-2)}`
+}
