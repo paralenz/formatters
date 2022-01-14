a7103b787370d37aade0b62760c88cdedb51a2ef
Carsten Daurehøj <arelstone@gmail.com>
2022-01-14 08:04:22 +0100

    ✏️ Change constant name to KILO_BYTE (#2)

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -2,4 +2,4 @@ export declare const BAR_TO_PSI_RATIO = 14.5037738;
 export declare const METERS_TO_FEET_RATIO = 3.28084;
 export declare const LBS_TO_KG_RATIO = 2.20462262185;
 export declare const KELVIN = 273.15;
-export declare const ONE_BYTE = 1024;
+export declare const ONE_KILOBYTE = 1024;
