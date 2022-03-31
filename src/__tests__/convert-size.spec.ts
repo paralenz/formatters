b556e6e1de0c2f1c3f8c8594b2ca0eb2285db4b5
Carsten Daurehøj <arelstone@gmail.com>
2022-03-31 08:28:39 +0200

    Joaõ is my man

--- src/__tests__/convert-size.spec.ts
+++ src/__tests__/convert-size.spec.ts
@@ -2,43 +2,52 @@ import { ConvertFileSize } from '../convert-file-size'
 
 describe('ConvertFileSize', () => {
   it('bytes->bytes', () => {
-    expect(new ConvertFileSize().from('B').to('B').convert(1)).toEqual(1)
+    expect(new ConvertFileSize().from('B').to('B').convert(1)).toEqual('1')
   });
   
   it('KB->KB', () => {
-    expect(new ConvertFileSize().from('KB').to('KB').convert(1)).toEqual(1)
+    expect(new ConvertFileSize().from('KB').to('KB').convert(1)).toEqual('1')
   });
   
   it('byte->KB', () => {
-    expect(new ConvertFileSize().from('B').to('KB').convert(1000)).toEqual(1)
+    expect(new ConvertFileSize().from('B').to('KB').convert(1000)).toBe('1')
   });
   
   it('KB->MB', () => {
-    expect(new ConvertFileSize().from('KB').to('MB').convert(1000)).toEqual(1)
+    expect(new ConvertFileSize().from('KB').to('MB').convert(1000)).toEqual('1')
   });
 
   it('MB->GB', () => {
-    expect(new ConvertFileSize().from('MB').to('GB').convert(1000)).toEqual(1)
+    expect(new ConvertFileSize().from('MB').to('GB').convert(1000)).toEqual('1')
   });
   
   it('KB->bytes', () => {
-    expect(new ConvertFileSize().from('KB').to('B').convert(1)).toEqual(1000)
+    expect(new ConvertFileSize().from('KB').to('B').convert(1)).toEqual('1024')
   });
   
   it('MB->KB', () => {
-    expect(new ConvertFileSize().from('MB').to('KB').convert(1)).toEqual(1000)
+    expect(new ConvertFileSize().from('MB').to('KB').convert(1)).toEqual('1024')
   });
   
   
   it('GB->MB', () => {
-    expect(new ConvertFileSize().from('GB').to('MB').convert(1)).toEqual(1000)
+    expect(new ConvertFileSize().from('GB').to('MB').convert(1)).toEqual('1024')
   });
   
   it('GB->KB', () => {
-    expect(new ConvertFileSize().from('GB').to('KB').convert(1)).toEqual(1e+6)
+    expect(new ConvertFileSize().from('GB').to('KB').convert(1)).toEqual('1048576')
   });
   
   it('GB->byte', () => {
-    expect(new ConvertFileSize().from('GB').to('B').convert(1)).toEqual(1e+9)
+    expect(new ConvertFileSize().from('GB').to('B').convert(1)).toEqual('1073741824')
+  });
+
+  it('should should append the unit', () => {
+    expect(new ConvertFileSize().from('KB').to('MB').appendUnit().convert(1000)).toEqual('1 MB')
+  });
+
+  it('should should append X decimals', () => {
+    expect(new ConvertFileSize().from('KB').to('MB').decimals(3).convert(1000)).toEqual('0.977')
+    expect(new ConvertFileSize().from('KB').to('MB').decimals(1).convert(1000)).toEqual('1.0')
   });
 })
--- src/convert-file-size.ts
+++ src/convert-file-size.ts
@@ -1,10 +1,13 @@
+import { ONE_KILOBYTE } from '.'
+
 type Unit = 'GB' | 'MB' | 'KB' | 'B' | 'TB'
 
 export class ConvertFileSize {
   private fromUnit: Unit = 'B'
   private toUnit: Unit = 'B'
   private readonly units: Unit[] = ['B', 'KB', 'MB', 'GB', 'TB']
-  private readonly baseSize = 1000
+  private postUnit = false
+  private postDecimals = 0
 
   from (unit: Unit): this {
     this.fromUnit = unit
@@ -18,19 +21,33 @@ export class ConvertFileSize {
     return this
   }
 
-  convert (size: number) {
+  decimals (num: number = this.postDecimals) {
+    this.postDecimals = num
+    return this
+  }
+
+  appendUnit () {
+    this.postUnit = true
+    return this
+  }
+
+  convert (size: number): string {
     const fromUnit = this.units.indexOf(this.fromUnit)
     const toUnit = this.units.indexOf(this.toUnit)
     let result: number | string = 0
 
     if (fromUnit < 0 || toUnit < 0) throw new Error('Error: Incorrect units')
-    if (fromUnit === toUnit) return size
 
-    result = fromUnit < toUnit
-      ? size / (Math.pow(this.baseSize, (toUnit - fromUnit)))
-      : size * (Math.pow(this.baseSize, (fromUnit - toUnit)))
-
-    return result
+    if (fromUnit === toUnit) {
+      result = size
+    } else {
+      result = fromUnit < toUnit
+        ? size / (Math.pow(ONE_KILOBYTE, (toUnit - fromUnit)))
+        : size * (Math.pow(ONE_KILOBYTE, (fromUnit - toUnit)))
     }
+
+    return this.postUnit
+      ? `${result.toFixed(this.postDecimals)} ${this.toUnit}`
+      : `${result.toFixed(this.postDecimals)}`
   }
 }
